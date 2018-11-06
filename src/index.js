eaa321b560dd913aea0c220dec7b964eb6736fed
fczbkk <riki@fczbkk.com>
2018-11-06 13:08:50 +0100

    chore: reformat source code for eslint

--- src/index.js
+++ src/index.js
@@ -1,45 +1,56 @@
 const relative_time_units = {
-  second : 1000,
-  minute : 1000 * 60,
-  hour   : 1000 * 60 * 60,
-  day    : 1000 * 60 * 60 * 24,
-  week   : 1000 * 60 * 60 * 24 * 7,
-  month  : 1000 * 60 * 60 * 24 * 30,
-  year   : 1000 * 60 * 60 * 24 * 365
+  second: 1000,
+  minute: 1000 * 60,
+  hour: 1000 * 60 * 60,
+  day: 1000 * 60 * 60 * 24,
+  week: 1000 * 60 * 60 * 24 * 7,
+  month: 1000 * 60 * 60 * 24 * 30,
+  year: 1000 * 60 * 60 * 24 * 365
 };
-
 
 const units = Object.keys(relative_time_units).join('|');
 
+const re_content = ''
+  // any whitespace
+  + '^\\s*'
+  // (optional) operator, positive or negative sign
+  + '(\\+|\\-)*'
+  // any whitespace
+  + '\\s*'
+  // (required) value
+  + '(\\d+)'
+  // any whitespace
+  + '\\s*'
+  // (required) unit
+  + '(' + units + ')'
+  // plural
+  + 's*'
+  // any whitespace
+  + '\\s*$';
 
-const re = new RegExp(
-  '^\\s*'           +       // any whitespace
-  '(\\+|\\-)*'      +       // (optional) operator, positive or negative sign
-  '\\s*'            +       // any whitespace
-  '(\\d+)'          +       // (required) value
-  '\\s*'            +       // any whitespace
-  '(' + units + ')' +       // (required) unit
-  's*'              +       // plural
-  '\\s*$'                   // any whitespace
-);
+const re = new RegExp(re_content);
 
-
+/**
+ * Parses value and unit from human-readable relative time.
+ * @param {string} input
+ * @return {{value: number, unit: string} | null} Returns `null` if input can not be parsed.
+ */
 function parseInput (input) {
   const match = re.exec(input);
 
   if (match !== null) {
     /* eslint-disable no-unused-vars */
-    let [original, operator, value, unit] = match;
+    const [, operator, value, unit] = match;
     /* eslint-enable no-unused-vars */
 
-    value = parseInt(value, 10);
+    let sanitized_value = parseInt(value, 10);
 
     if (operator === '-') {
-      value = value * -1;
+      sanitized_value = sanitized_value * -1;
     }
 
     return {
-      value: value,
+      value: sanitized_value,
       unit: unit
     };
 
@@ -48,12 +59,11 @@ function parseInput (input) {
   return null;
 }
 
-
 /**
  * Parse simple relative time in human readable format to milliseconds.
  * @name parseRelativeTime
  * @param {string} input Human readable format of relative time
- * @returns {null|number}
+ * @returns {null | number}
  * @example
  * parseRelativeTime('2 days');  // --> 172800000
  */
